83cba47de462f6dbf1ad038c24549febd1bbfe1c
boslaw <yaroslawbonarets@gmail.com>
2019-04-06 16:21:52 +0300

    @ignoreidea

---
+++ dist/precache-manifest.667d4707e12928876f623fd819d1798c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7c849ad540c6c2a0455",
+    "url": "/js/about.ad763791.js"
+  },
+  {
+    "revision": "0b460586bd0b79fc69c2",
+    "url": "/css/app.19826125.css"
+  },
+  {
+    "revision": "0b460586bd0b79fc69c2",
+    "url": "/js/app.0403f7ca.js"
+  },
+  {
+    "revision": "f06e1abfddab9297c1c3",
+    "url": "/js/chunk-vendors.8681114a.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "cf3477c38565d1b5fc6f6fdc23f60e55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
