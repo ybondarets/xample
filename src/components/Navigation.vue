ae9cdf6a03e69e6fb5245fc2a21a229c40f4c2da
boslaw <yaroslawbonarets@gmail.com>
2019-04-09 09:22:56 +0300

    @routerConfiguration

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -11,6 +11,15 @@
     };
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+    #nav {
+        padding: 30px;
+        a {
+            font-weight: bold;
+            color: #2c3e50;
+            &.router-link-exact-active {
+                 color: #42b983;
+             }
+        }
+    }
 </style>
